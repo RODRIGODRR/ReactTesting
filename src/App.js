c1193b9b421592337e3957cf6a4b094bc9ee2a6f
Rodrigo Silva <rodridrr_04@hotmail.com>
2019-06-09 11:32:58 -0300

    simulando merge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
+// aqui 1234567890
+// simulando merge;
 class App extends Component {
   render() {
     return (
